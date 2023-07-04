5851acb53cc9a3b9ebb1284f7352179a2eac80a9
Zimbini9617 <126578349+Zimbini9617@users.noreply.github.com>
2023-07-04 09:55:12 +0200

    update

---
+++ Nesting/B_two_dimensional_arrays/two-sum-pairs.js
@@ -0,0 +1,26 @@
+// Write a function `twoSumPairs(numbers, target)` that accepts an array of numbers and a target number
+// as arguments. The function should return a 2D array containing all unique pairs of elements that
+// sum to the target.
+
+let twoSumPairs = function(numbers, target) {
+  let pairs = [];
+
+  for (let i = 0; i < numbers.length; i++) {
+    let num1 = numbers[i];
+
+    for (let j = i + 1; j < numbers.length; j++) {
+      let num2 = numbers[j];
+
+      if (num1 + num2 === target) {
+        let pair = [ num1, num2 ];
+        pairs.push(pair);
+      }
+    }
+  }
+  return pairs;
+}
+
+console.log(twoSumPairs([2, 3, 4, 6, 5], 8)); 
+console.log(twoSumPairs([10, 7, 4, 5, 2], 12)); 
+console.log(twoSumPairs([3, 9, 8], 11)); 
+console.log(twoSumPairs([3, 9, 8], 10)); // [ ]
